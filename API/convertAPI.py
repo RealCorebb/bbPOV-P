f52ebc956035d2d108305d85c5fa15057f9352f7
coreb <coreoobb@qq.com>
2020-12-01 12:02:13 +0800

    Convert API

--- API/convertAPI.py
+++ API/convertAPI.py
@@ -6,24 +6,16 @@ import numpy as np
 import os
 import math
 from PIL import Image
- 
-#配置
-#Frame = 5 #指定帧数量
-NUMPIXELS = 80 #单边LED数量
-Div = 240 #1圈分割数
- 
-
-gif_file_name = "magic.gif"
-im = Image.open(gif_file_name)
-print(im.is_animated)
-print(im.n_frames)
-Frame=im.n_frames
-
-
- 
+import base64
+from io import BytesIO
+import flask
+from flask import request,json,make_response
+import gzip
+app = flask.Flask(__name__)
+data = []
 
 #画像変換関数
-def polarConv(imgOrgin, frame):
+def polarConv(imgOrgin, frame,NUMPIXELS,Div):
     
     h = imgOrgin.height #帧尺寸
     w = imgOrgin.width
@@ -55,18 +47,44 @@ def polarConv(imgOrgin, frame):
             bP = imgArray[hC + math.ceil(i * math.cos(2*math.pi/Div*j)),
                          wC - math.ceil(i * math.sin(2*math.pi/Div*j)), 2]
             imgPolar.putpixel((i,j), (rP, gP, bP))
+    buffered = BytesIO()
+    imgPolar.save(buffered,format="BMP") #输出极坐标变换后的图像
+    data.append(base64.b64encode(buffered.getvalue()).decode('utf-8'))        
 
-    imgPolar.save(str(frame)+'.bmp') #输出极坐标变换后的图像
-            
+@app.route('/', methods=['POST'])
+def convert():
+    data.clear()
+    gif_file_name = request.files['img']
+    NUMPIXELS = int(request.form['NUMPIXELS']) #半径灯条数
+    Div = int(request.form['Div']) #1圈分割数
+    im = Image.open(gif_file_name)
+    print(im.is_animated)
+    print(im.n_frames)
+    Frame=im.n_frames
+    for i in range(Frame):
+        #输出每一帧
+        frame = im.convert('RGBA') #如果是RGB的话，有的透明背景GIF不兼容
+        polarConv(frame, i, NUMPIXELS, Div)
+        if i != Frame-1:
+            im.seek(im.tell()+1)
+    content = gzip.compress(json.dumps(data).encode('utf8'),5)    
+    response = make_response(content)
+    response.headers['Content-Type'] = 'application/json'
+    response.headers['Content-length'] = len(content)
+    response.headers['Content-Encoding'] = 'gzip'
+    return response
+app.run()
+
+
+
+
+
+
+ 
+
+
  
 
     
-    
-for i in range(Frame):
-    #输出每一帧
-    frame = im.convert('RGBA') #如果是RGB的话，有的透明背景GIF不兼容
-    polarConv(frame, i)
-    if i != Frame-1:
-        im.seek(im.tell()+1)
 
     
